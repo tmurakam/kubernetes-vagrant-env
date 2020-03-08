c752db860d96218fba963fce086fadfb2b06a9c9
Takuya Murakami <tmurakam@tmurakam.org>
2020-03-08 22:02:09 +0900

    centos8 test

---
+++ centos8/scripts/bootstrap.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+cat << EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# Install docker /kubeadm
+sudo yum install -y docker kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+sudo systemctl enable --now docker
+sudo systemctl enable --now kubelet
+
+# Disable swap
+sudo swapoff -a
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
